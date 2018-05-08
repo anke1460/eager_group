3c6aba7eba6b0b7405800b36babfa138ada66851
anke1460 <zuosjob@gmail.com>
2018-05-08 14:13:29 +0800

    suport rails 5

--- lib/eager_group/active_record_relation.rb
+++ lib/eager_group/active_record_relation.rb
@@ -1,14 +1,16 @@
+module OverQuery
+ def exec_queries
+    if eager_group_values.present?
+      EagerGroup::Preloader.new(self.klass, super, eager_group_values).run
+    end
+    super
+  end
+end
+
 class ActiveRecord::Relation
   # Post.all.eager_group(:approved_comments_count, :comments_average_rating)
 
-  def exec_queries_with_eager_group
-    records = exec_queries_without_eager_group
-    if eager_group_values.present?
-      EagerGroup::Preloader.new(self.klass, records, eager_group_values).run
-    end
-    records
-  end
-  alias_method_chain :exec_queries, :eager_group
+  prepend OverQuery
 
   def eager_group(*args)
     check_if_method_has_arguments!('eager_group', args)
